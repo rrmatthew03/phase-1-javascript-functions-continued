82915ccbcffb67261bcd4e83707d6685df16541e
ryan rivera <rrmatthew03@yahoo.com>
2022-03-25 18:12:47 -0700

    commit tree 4ac290570a310c15884149d4a5f93085cb03ad0c

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // code your solution here
+function saturdayFun(){
+    console.log('roller-skate!')
+}
+saturdayFun()
+
+function saturdayFun(){
+    console.log('roller-skate')
+    return ('This Saturday, I want to roller-skate!')
+}
+
+function saturdayFun(){
+    console.log('bathe my dog!')
+    return ('This Saturday, I want to bathe my dog!')
+}
+saturdayFun()
+
+function mondayWork(){
+    console.log('go to the office')
+}
+mondayWork()
+
+function mondayWork(){
+    return ('This Monday, I will go to the office.')
+}
+
+mondayWork()
+
+function mondayWork(){
+    console.log('work from home')
+    return ('This Monday, I will work from home.')
+}
+mondayWork()
+
+function wrapAdjective(){
+    console.log('You are, speacial')
+}
+wrapAdjective()
+
+function wrapAdjective(empathetic){
+let result = wrapAdjective('*')
+let emphatic = result("a hard worker")
+    result ('You are *a hard worker*!')
+}
+wrapAdjective('empathetic')
